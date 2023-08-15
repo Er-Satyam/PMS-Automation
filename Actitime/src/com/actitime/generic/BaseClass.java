fdca6993cd0086b7cbdf99bed1002472859f123c
Satyamgit <satyamsharma1296@gmail.com>
2023-08-16 00:10:03 +0530

    hi

--- Actitime/src/com/actitime/generic/BaseClass.java
+++ Actitime/src/com/actitime/generic/BaseClass.java
@@ -17,49 +17,40 @@ import com.actitime.pom.TaskListPage;
 //baseclass contain all configuration methods which is common for all testcases
 
 public class BaseClass {
-   public static WebDriver driver;//static mandatory for screenshot
+	public static WebDriver driver;// static mandatory for screenshot
+
 	@BeforeTest
 	public void openBrowser() {
-	Reporter.log("Open Browser",true);
-	driver=new ChromeDriver();
-	driver.manage().window().maximize();
-	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		Reporter.log("Open Browser", true);
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 	}
-	
-	
-	
+
 	@AfterTest
 	public void closeBrowser() {
-		Reporter.log("Close Browser",true);	
+		Reporter.log("Close Browser", true);
 		driver.quit();
 	}
-	
-	
+
 	@BeforeMethod
-	public void login() throws IOException{
-		Reporter.log("Login",true);
-		FileLib f=new FileLib();
+	public void login() throws IOException {
+		Reporter.log("Login", true);
+		FileLib f = new FileLib();
 		String url = f.getPropertyData("url");
 		String un = f.getPropertyData("username");
 		String pw = f.getPropertyData("password");
 		driver.get(url);
-		LoginPage l= new LoginPage(driver);
+		LoginPage l = new LoginPage(driver);
 		l.setLogin(un, pw);
-		
-	
-	   
-	
+
 	}
-	
-	
-	
+
 	@AfterMethod
 	public void logout() throws InterruptedException {
-		Reporter.log("Logout",true);
-		HomePage h=new HomePage(driver);
+		Reporter.log("Logout", true);
+		HomePage h = new HomePage(driver);
 		h.setLogout();
 	}
-	
-	
-	
+
 }
